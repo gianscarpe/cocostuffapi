d6761a60da90431c72cf7fc2b41f899955ac4e1a
gianscarpe <gianluca@scarpellini.dev>
2021-12-20 15:29:59 +0100

    fix

--- PythonAPI/pycocotools/cocostuffeval.py
+++ PythonAPI/pycocotools/cocostuffeval.py
@@ -1,9 +1,12 @@
-__author__ = 'hcaesar'
+__author__ = "hcaesar"
 
-import numpy as np
 import datetime
 import time
-from cocostuffhelper import cocoSegmentationToSegmentationMap
+
+import numpy as np
+
+from .cocostuffhelper import cocoSegmentationToSegmentationMap
+
 
 class COCOStuffeval:
     # Internal functions for evaluating stuff segmentations against a ground-truth.
@@ -42,7 +45,7 @@ class COCOStuffeval:
     # Licensed under the Simplified BSD License [see coco/license.txt]
 
     def __init__(self, cocoGt, cocoRes, stuffStartId=92, stuffEndId=182, addOther=True):
-        '''
+        """
         Initialize COCOStuffeval using COCO APIs for gt and dt
         :param cocoGt: COCO object with ground truth annotations
         :param cocoRes: COCO object with detection results
@@ -50,93 +53,115 @@ class COCOStuffeval:
         :param stuffEndId: id of the last stuff class
         :param addOther: whether to use a other class
         :return: None
-        '''
-        self.cocoGt   = cocoGt              # Ground truth COCO API
-        self.cocoRes   = cocoRes            # Result COCO API
-        self.stuffStartId = stuffStartId    # Id of the first stuff class
-        self.stuffEndId = stuffEndId        # Id of the last stuff class
-        self.addOther = addOther            # Whether to add a class that subsumes all thing classes
+        """
+        self.cocoGt = cocoGt  # Ground truth COCO API
+        self.cocoRes = cocoRes  # Result COCO API
+        self.stuffStartId = stuffStartId  # Id of the first stuff class
+        self.stuffEndId = stuffEndId  # Id of the last stuff class
+        self.addOther = (
+            addOther  # Whether to add a class that subsumes all thing classes
+        )
 
-        self.eval     = {}                  # Accumulated evaluation results
-        self.confusion = []                 # Confusion matrix that all metrics are computed on
-        self.stats = []                     # Result summarization
-        self.statsClass = {}                # Per-class results
-        self.params = Params()              # Evaluation parameters
-        self.params.imgIds = sorted(cocoGt.getImgIds()) # By default we use all images from the GT file
-        self.catIds = range(stuffStartId, stuffEndId+addOther+1) # Take into account all stuff
-                                                                 # classes and one 'other' class
+        self.eval = {}  # Accumulated evaluation results
+        self.confusion = []  # Confusion matrix that all metrics are computed on
+        self.stats = []  # Result summarization
+        self.statsClass = {}  # Per-class results
+        self.params = Params()  # Evaluation parameters
+        self.params.imgIds = sorted(
+            cocoGt.getImgIds()
+        )  # By default we use all images from the GT file
+        self.catIds = range(
+            stuffStartId, stuffEndId + addOther + 1
+        )  # Take into account all stuff
+        # classes and one 'other' class
 
     def evaluate(self):
-        '''
+        """
         Run per image evaluation on given images and store results in self.confusion.
         Note that this can take up to several hours.
         :return: None
-        '''
+        """
 
         # Reset eval and print message
         tic = time.time()
         imgIds = self.params.imgIds
-        print('Evaluating stuff segmentation on %d images and %d classes...' \
-            % (len(imgIds), len(self.catIds)))
+        print(
+            "Evaluating stuff segmentation on %d images and %d classes..."
+            % (len(imgIds), len(self.catIds))
+        )
 
         # Check that all images in params occur in GT and results
         gtImgIds = sorted(set(self.cocoGt.getImgIds()))
-        resImgIds = sorted(set([a['image_id'] for a in self.cocoRes.anns.values()]))
+        resImgIds = sorted(set([a["image_id"] for a in self.cocoRes.anns.values()]))
         missingInGt = [p for p in imgIds if p not in gtImgIds]
         missingInRes = [p for p in imgIds if p not in resImgIds]
         if len(missingInGt) > 0:
-            raise Exception('Error: Some images specified in imgIds do not occur in the GT: %s' % missingInGt)
+            raise Exception(
+                "Error: Some images specified in imgIds do not occur in the GT: %s"
+                % missingInGt
+            )
         if len(missingInRes) > 0:
-            raise Exception('Error: %d evaluation images not found in the result!' % len(missingInRes))
+            raise Exception(
+                "Error: %d evaluation images not found in the result!"
+                % len(missingInRes)
+            )
 
         # Create confusion matrix
         labelCount = max([c for c in self.cocoGt.cats])
         confusion = np.zeros((labelCount, labelCount))
         for i, imgId in enumerate(imgIds):
-            if i+1 == 1 or i+1 == len(imgIds) or (i+1) % 10 == 0:
-                print('Evaluating image %d of %d: %d' % (i+1, len(imgIds), imgId))
-            confusion = self._accumulateConfusion(self.cocoGt, self.cocoRes, confusion, imgId)
+            if i + 1 == 1 or i + 1 == len(imgIds) or (i + 1) % 10 == 0:
+                print("Evaluating image %d of %d: %d" % (i + 1, len(imgIds), imgId))
+            confusion = self._accumulateConfusion(
+                self.cocoGt, self.cocoRes, confusion, imgId
+            )
         self.confusion = confusion
 
         # Set eval struct to be used later
         self.eval = {
-            'params': self.params,
-            'date': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            'confusion': self.confusion
+            "params": self.params,
+            "date": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "confusion": self.confusion,
         }
 
         toc = time.time()
-        print('DONE (t={:0.2f}s).'.format(toc-tic))
+        print("DONE (t={:0.2f}s).".format(toc - tic))
 
     def _accumulateConfusion(self, cocoGt, cocoRes, confusion, imgId):
-        '''
+        """
         Accumulate the pixels of the current image in the specified confusion matrix.
-        Note: For simplicity we do not map the labels to range [0, L-1], 
+        Note: For simplicity we do not map the labels to range [0, L-1],
               but keep the original indices when indexing 'confusion'.
         :param cocoGt: COCO object with ground truth annotations
         :param cocoRes: COCO object with detection results
         :param confusion: confusion matrix that will be modified
         :param imgId: id of the current image
         :return: confusion (modified confusion matrix)
-        '''
+        """
 
         # Combine all annotations of this image in labelMapGt and labelMapRes
-        labelMapGt  = cocoSegmentationToSegmentationMap(cocoGt,  imgId, includeCrowd=False)
-        labelMapRes = cocoSegmentationToSegmentationMap(cocoRes, imgId, includeCrowd=False)
+        labelMapGt = cocoSegmentationToSegmentationMap(
+            cocoGt, imgId, includeCrowd=False
+        )
+        labelMapRes = cocoSegmentationToSegmentationMap(
+            cocoRes, imgId, includeCrowd=False
+        )
 
         # Check that the result has only valid labels
         invalidLabels = [l for l in np.unique(labelMapRes) if l not in self.catIds]
         if len(invalidLabels) > 0:
-            raise Exception('Error: Invalid classes predicted in the result file: %s. Please insert only labels in the range [%d, %d]!'
-            % (str(invalidLabels), min(self.catIds), max(self.catIds)))
-        
+            raise Exception(
+                "Error: Invalid classes predicted in the result file: %s. Please insert only labels in the range [%d, %d]!"
+                % (str(invalidLabels), min(self.catIds), max(self.catIds))
+            )
+
         # Filter labels that are not in catIds (includes the 0 label)
         valid = np.reshape(np.in1d(labelMapGt, self.catIds), labelMapGt.shape)
         validGt = labelMapGt[valid].astype(int)
         validRes = labelMapRes[valid].astype(int)
 
         # Gather annotations in confusion matrix
-        #for g, d in zip(validGt, validRes):
+        # for g, d in zip(validGt, validRes):
         #    confusion[g-1, d-1] += 1
 
         # Much faster version using np.unique
@@ -151,14 +176,14 @@ class COCOStuffeval:
         return confusion
 
     def summarize(self):
-        '''
+        """
         Compute and display the metrics for leaf nodes and super categories.
         :return: tuple of (general) stats and (per-class) statsClass
-        '''
+        """
 
         # Check if evaluate was run and then compute performance metrics
         if not self.eval:
-            raise Exception('Error: Please run evaluate() first!')
+            raise Exception("Error: Please run evaluate() first!")
 
         # Compute confusion matrix for supercategories
         confusion = self.confusion
@@ -166,39 +191,41 @@ class COCOStuffeval:
 
         # Compute performance
         [miou, fwiou, macc, pacc, ious, maccs] = self._computeMetrics(confusion)
-        [miouSup, fwiouSup, maccSup, paccSup, iousSup, maccsSup] = self._computeMetrics(confusionSup)
+        [miouSup, fwiouSup, maccSup, paccSup, iousSup, maccsSup] = self._computeMetrics(
+            confusionSup
+        )
 
         # Store metrics
         stats = np.zeros((8,))
-        stats[0] = self._printSummary('Mean IOU', 'leaves', miou)
-        stats[1] = self._printSummary('FW IOU', 'leaves', fwiou)
-        stats[2] = self._printSummary('Mean accuracy', 'leaves', macc)
-        stats[3] = self._printSummary('Pixel accuracy', 'leaves', pacc)
-        stats[4] = self._printSummary('Mean IOU', 'supercats', miouSup)
-        stats[5] = self._printSummary('FW IOU', 'supercats', fwiouSup)
-        stats[6] = self._printSummary('Mean accuracy', 'supercats', maccSup)
-        stats[7] = self._printSummary('Pixel accuracy', 'supercats', paccSup)
+        stats[0] = self._printSummary("Mean IOU", "leaves", miou)
+        stats[1] = self._printSummary("FW IOU", "leaves", fwiou)
+        stats[2] = self._printSummary("Mean accuracy", "leaves", macc)
+        stats[3] = self._printSummary("Pixel accuracy", "leaves", pacc)
+        stats[4] = self._printSummary("Mean IOU", "supercats", miouSup)
+        stats[5] = self._printSummary("FW IOU", "supercats", fwiouSup)
+        stats[6] = self._printSummary("Mean accuracy", "supercats", maccSup)
+        stats[7] = self._printSummary("Pixel accuracy", "supercats", paccSup)
 
         # Store statsClass
         statsClass = {
-            'ious': ious,
-            'maccs': maccs,
-            'iousSup': iousSup,
-            'maccsSup': maccsSup
+            "ious": ious,
+            "maccs": maccs,
+            "iousSup": iousSup,
+            "maccsSup": maccsSup,
         }
         self.stats, self.statsClass = stats, statsClass
 
         return stats, statsClass
 
     def _getSupCatConfusion(self, confusion):
-        '''
+        """
         Maps the leaf category confusion matrix to a super category confusion matrix.
         :param confusion: leaf category confusion matrix
         :return: confusionSup (super category confusion matrix)
-        '''
+        """
 
         # Retrieve supercategory mapping
-        supCats = [c['supercategory'] for c in self.cocoGt.cats.values()]
+        supCats = [c["supercategory"] for c in self.cocoGt.cats.values()]
         supCatsUn = sorted(set(supCats))
         keys = supCatsUn
         vals = range(0, len(supCatsUn))
@@ -210,8 +237,16 @@ class COCOStuffeval:
         confusionSup = np.zeros((supCatCount, supCatCount))
         for supCatIdA in xrange(0, supCatCount):
             for supCatIdB in xrange(0, supCatCount):
-                curLeavesA = np.where([s == supCatIdA for s in supCatIds])[0] + self.stuffStartId - 1
-                curLeavesB = np.where([s == supCatIdB for s in supCatIds])[0] + self.stuffStartId - 1
+                curLeavesA = (
+                    np.where([s == supCatIdA for s in supCatIds])[0]
+                    + self.stuffStartId
+                    - 1
+                )
+                curLeavesB = (
+                    np.where([s == supCatIdB for s in supCatIds])[0]
+                    + self.stuffStartId
+                    - 1
+                )
                 confusionLeaves = confusion[curLeavesA, :]
                 confusionLeaves = confusionLeaves[:, curLeavesB]
                 confusionSup[supCatIdA, supCatIdB] = confusionLeaves.sum()
@@ -220,11 +255,11 @@ class COCOStuffeval:
         return confusionSup
 
     def _computeMetrics(self, confusion):
-        '''
+        """
         Compute evaluation metrics given a confusion matrix.
         :param confusion: any confusion matrix
         :return: tuple (miou, fwiou, macc, pacc, ious, maccs)
-        '''
+        """
 
         # Init
         labelCount = confusion.shape[0]
@@ -236,17 +271,19 @@ class COCOStuffeval:
         # Get true positives, positive predictions and positive ground-truth
         total = confusion.sum()
         if total <= 0:
-            raise Exception('Error: Confusion matrix is empty!')
+            raise Exception("Error: Confusion matrix is empty!")
         tp = np.diagonal(confusion)
         posPred = confusion.sum(axis=0)
         posGt = confusion.sum(axis=1)
-        
+
         # Check which classes have elements
         valid = posGt > 0
         iousValid = np.logical_and(valid, posGt + posPred - tp > 0)
 
         # Compute per-class results and frequencies
-        ious[iousValid] = np.divide(tp[iousValid], posGt[iousValid] + posPred[iousValid] - tp[iousValid])
+        ious[iousValid] = np.divide(
+            tp[iousValid], posGt[iousValid] + posPred[iousValid] - tp[iousValid]
+        )
         maccs[valid] = np.divide(tp[valid], posGt[valid])
         freqs = np.divide(posGt, total)
 
@@ -259,20 +296,21 @@ class COCOStuffeval:
         return miou, fwiou, macc, pacc, ious, maccs
 
     def _printSummary(self, titleStr, classStr, val):
-        '''
+        """
         Prints the current metric title, class type and value.
         :param titleStr: a string that represents the name of the metric
         :param classStr: the type of classes the metric was performed on (leaves/supercategories)
         :param val: the value of the metric
-        '''
-        iStr = ' {:<14} @[ classes={:>8s} ] = {:0.4f}'
+        """
+        iStr = " {:<14} @[ classes={:>8s} ] = {:0.4f}"
         print(iStr.format(titleStr, classStr, val))
         return val
 
+
 class Params:
-    '''
+    """
     Params for coco stuff evaluation api
-    '''
+    """
 
     def __init__(self):
         self.imgIds = []
